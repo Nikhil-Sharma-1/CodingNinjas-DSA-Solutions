1852accbe70fcd4baa543a1e6935c2bbd444a809
Nikhil Sharma <sharmanikhil67696@gmail.com>
2021-01-26 09:30:00 -0800

    Create TowerOfHanoi.java

---
+++ Assignment: Recursion 1/TowerOfHanoi.java
@@ -0,0 +1,16 @@
+
+public class solution {
+
+	public static void towerOfHanoi(int disks, char source, char auxiliary, char destination) {
+		
+        if(disks == 0) {
+            
+            return;
+        }
+        
+        towerOfHanoi(disks - 1, source, destination, auxiliary);
+        System.out.println(source + " " + destination);
+        towerOfHanoi(disks - 1, auxiliary, source, destination);
+
+	}
+}
