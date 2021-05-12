e4a7113e465f455f3409207694166e1aaf022433
Nikhil Sharma <sharmanikhil67696@gmail.com>
2021-05-12 08:54:58 -0700

    Create Code : Merge two sorted LL.java

---
+++ Linked List 2/Code : Merge two sorted LL.java
@@ -0,0 +1,47 @@
+public class Solution {
+    
+    public static LinkedListNode<Integer> mergeTwoSortedLinkedLists(LinkedListNode<Integer> head1, LinkedListNode<Integer> head2) {
+        
+        if(head1 == null) {
+            return head2;
+        }
+        
+        if(head2 == null) {
+            return head1;
+        }
+        
+        LinkedListNode<Integer> head, tail;
+        if(head1.data <= head2.data) {
+            head = head1;
+            tail = head1;
+            head1 = head1.next;
+        } else {
+             head = head2;
+             tail = head2;
+             head2 = head2.next;
+        }
+        
+        while(head1 != null && head2 != null) {
+            if(head1.data <= head2.data) {
+                tail.next = head1;
+                tail = tail.next;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                tail = tail.next;
+                head2 = head2.next;
+            }
+        }
+        
+        if(head1 == null) {
+            tail.next = head2;
+        }
+        
+        if(head2 == null) {
+           tail.next = head1;
+        }
+        
+        return head;
+    }
+	
+}
